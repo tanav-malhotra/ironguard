f8fee686e203c3ba43e826cfb7b544f0e2e260fc
Tanav Malhotra <tanavm2009@gmail.com>
2025-10-14 04:31:26 -0400

    fix: update import path from "ironguard_ai" to "ironguard" in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use ironguard_ai::cli;
+use ironguard::cli; // remove when dir name is changed from ironguard_ai to ironguard
 
 #[tokio::main(flavor = "multi_thread")] 
 async fn main() -> anyhow::Result<()> {
